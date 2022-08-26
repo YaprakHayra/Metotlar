f55bdbfb8d8d46b0328a00db8d2c34332e24fd16
HP <HP@YAPRAK>
2022-08-26 18:29:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Metotlar;
+
+
+internal class Program
+{
+
+
+
+
+    public static void Main(string[] args)
+    {
+        Urun urun1 = new Urun();
+       
+        urun1.Adi = "Elma";
+        urun1.Fiyati = 15;
+        urun1.Aciklama = "amasya elması";
+
+        Urun urun2 = new Urun();
+        
+        urun2.Adi = "çilek";
+        urun2.Fiyati = 20;
+        urun2.Aciklama = "Tatlı çilek";
+
+        Urun urun3 = new Urun();
+        
+        urun3.Adi = "karpuz";
+        urun3.Fiyati = 80;
+        urun3.Aciklama = "Diyarbakır karpuzu";
+
+        Urun[] urunler = new Urun[] {urun1,urun2,urun3 };
+
+        foreach (Urun x in urunler)
+        {
+            Console.WriteLine(x.Adi);
+            Console.WriteLine(x.Fiyati);
+            Console.WriteLine(x.Aciklama);
+            Console.WriteLine("----");
+        }
+        Console.WriteLine("----METOTLAR----");
+        
+
+        //instance - örnek
+        //encapsulation
+        SepetManager sepetManager = new SepetManager();
+        sepetManager.Ekle(urun1);
+        sepetManager.Ekle(urun2);
+        sepetManager.Ekle(urun3);
+
+      //  sepetManager.Ekle2("armut","yeşil armut",12,5);
+     //   sepetManager.Ekle2("elma", "Amasya Elması", 12,23);
+        //sepetManager.Ekle2("KArpuz", "Diyarbakır", 12,44);
+
+    }
+    }
+    
+    
---
+++ SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class SepetManager
+    {
+        //naming convention
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine( "Sepete Eklendi: " + urun.Adi + urun.Aciklama+ urun.Fiyati+ urun.StokAdedi);
+        }
+       /* public void Ekle2(string urunAdi, string aciklama, double fiyat,int stokAdedi)
+        {
+            Console.WriteLine("Sepete Eklendi: "+ urunAdi+" "+aciklama+" "+fiyat);
+        }
+*/
+
+    }
+}
